03a48876b6cb821583191ac07035e6dd8378d4f3
Michael McFarland <gardengrove422@gmail.com>
2018-06-27 13:26:53 -0400

    Author Directive Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AuthorsService } from "./authors.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AuthorsComponent } from "./authors/authors.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, AuthorsComponent],
+  imports: [BrowserModule],
+  providers: [AuthorsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
